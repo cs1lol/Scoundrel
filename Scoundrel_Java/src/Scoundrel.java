c4dbb6b36235b9650a420ffb2da209d8603c319b
cs1lmao <cadensentz1@gmail.com>
2025-11-17 15:00:48 -0500

    Update Scoundrel.java

--- Scoundrel_Java/src/Scoundrel.java
+++ Scoundrel_Java/src/Scoundrel.java
@@ -2,6 +2,7 @@ import game.Game;
 
 public class Scoundrel {
     public static void main(String[] args) {
+        System.out.println("Welcome to Scoundrel V1\n\n");
         Game.gameStart();
         Game.gameEnd();
     }
